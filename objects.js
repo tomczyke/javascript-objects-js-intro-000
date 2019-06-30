07a16af3f14e63786ab6d097e12f952f481201dd
Evan Tomczyk <tomczykej@gmail.com>
2019-06-30 21:41:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,11 +4,9 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 
-  return playlist
+  return playlist;
 }
-var playlist = {
-  artistName: 'songTitle',
-};
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   playlist
